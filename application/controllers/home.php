a18daa9042f97aa9dba004808a1ea21a1c7ad70b
rhyrichard <richard.adam112@gmail.com>
2022-06-16 16:02:10 +0700

    Membuat Model untuk Home dan Login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,7 +6,7 @@ class Home extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('home_model', 'home');
+		$this->load->model('m_home', 'home');
 	}
 	
 	public function index()
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,7 +6,7 @@ class Login extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('login_model', 'login');
+		$this->load->model('m_login', 'login');
 	}
 	
 	public function index()
